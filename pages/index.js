3c05cac71dea853326b5cd2b0866b733c7a063ad
Nikola Zdravkovic <zdravko.dzoni@gmail.com>
2022-06-23 22:29:11 +0200

    next-app

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Act digtal website</title>
+        <meta name='keywords' content='web dev'/>
+      </Head>
+      <h1>Welcome to Act digital</h1>
+    </div>
+  )
+}
